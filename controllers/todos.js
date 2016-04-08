d6c67174b81e16c54672fd3cc7d901a7ffe4172f
Mike Hogarth <mikey@macbook.local>
2016-04-08 09:13:51 +0100

    Initial promisification

--- controllers/todos.js
+++ controllers/todos.js
@@ -6,17 +6,17 @@ const Todo = require('../models/todo');
  *  GET /todos
  */
 module.exports.index = function(req, res) { 
-  Todo.find({}, (err, todos) => res.send(err || todos));
+  Todo.find({}).then(todos => res.send(todos), handleError.bind(res)); 
 }
 
 /*
  * GET /todos/:id
  */
 module.exports.show = function(req, res) {
-  Todo.findById(req.params.id, function(err, todo) {
-    res.status(todo ? 200 : 404);
-    res.send(err || todo); 
-  });
+  Todo.findById(req.params.id).then(
+    todo => { res.send(todo) }, 
+    err  => { handleError(res, err, 404) }
+  )
 }
 
 /*
@@ -25,33 +25,39 @@ module.exports.show = function(req, res) {
 module.exports.update = function(req, res) {
   Todo.findById(req.params.id, function(err, todo) {
     todo.set(todoParams(req));
-    todo.save(function(err, todo) { 
-      res.status(err ? 400 : 200)
-      res.send(err || todo); 
-    });
+    todo.save().then(
+      todo => { res.send(todo) },
+      err  => { handleError(res, err, 400) }
+    );
   });
-};
+}
 
 /*
  * POST /todos
  */
 module.exports.create = function(req, res) {
   const todo = new Todo(todoParams(req));
-  todo.save(function(err, todo) {
-    res.status(err ? 400 : 201)
-    res.send(err || todo);
-  });
+  todo.save().then(
+    todo => {
+      res.status(201);
+      res.send(todo);
+    },
+    err  => { handleError(res, err, 400) }
+  )
 }
 
 /*
  * DELETE /todos/:id
  */ 
 module.exports.destroy = function(req, res) {
-  Todo.findById(req.params.id).remove(function(err, todo) {
-    res.status(err ? 404 : 204)
-    res.send(err || null); 
-  });
-};
+  Todo.findById(req.params.id).remove().then(
+    todo => { 
+      res.status(204);
+      res.send(null);
+    },
+    err  => { handleError(res, err, 404) }
+  )
+}
 
 
 /*
@@ -59,4 +65,9 @@ module.exports.destroy = function(req, res) {
  */
 function todoParams(req) {
   return req.body.todo;
+}
+
+function handleError(res, err, statusCode) {
+  res.status(statusCode || 500);
+  res.send(err);
 }
