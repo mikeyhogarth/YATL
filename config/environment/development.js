742de7436531a2a30391e8742837e6d9faeed04a
Mike Hogarth <mikehogarth20@hotmail.com>
2016-04-10 08:59:53 +0100

    Config refactoring

---
+++ config/environment/development.js
@@ -0,0 +1,7 @@
+require('dotenv').config();
+
+module.exports = {
+  env: "development",
+  port: 3000,
+  db_host: process.env.DB_HOST
+}
---
+++ config/environment/production.js
@@ -0,0 +1,4 @@
+module.exports = {
+  env: "production",
+  db_host: process.env.DB_HOST
+}
